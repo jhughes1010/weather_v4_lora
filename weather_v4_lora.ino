315b681c1cb42509ca294c8f353f550849518c71
James Hughes <jhughes1010@gmail.com>
2022-09-23 03:38:43 -0600

    initial commit

---
+++ weather_v4_lora.ino
@@ -0,0 +1,16 @@
+//LoRa baseed weather station
+//Hardware design by Debasish Dutta
+//Software design by James Hughes
+
+void setup()
+{
+
+}
+
+//===================================================
+// loop: these are not the droids you are looking for
+//===================================================
+void loop()
+{
+  //no loop code
+}
